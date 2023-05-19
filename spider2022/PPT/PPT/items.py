83dc8258d6a5a36bb13d7e65134818ba2961cf88
DoubleSails <48173376+Double-Sail@users.noreply.github.com>
2023-05-20 00:04:50 +0800

    Delete spider2022 directory

--- spider2022/PPT/PPT/items.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-"""
-用来定义索要获取数据的形式
-"""
-class PptItem(scrapy.Item):
-    
-    pass
--- spider2022/PPT/PPT/pipelines.py
+++
@@ -1,7 +0,0 @@
-# scrapy针对文件类爬取，设置了不同的包，有文件、图片、视频等。
-from scrapy.pipelines.files import FilesPipeline
-import scrapy
-
-class PptPipeline:
-    def process_item(self, item, spider):
-        return item
